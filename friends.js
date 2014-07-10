00e11bfb54d7e276f7a5fce97ce7d522abd28a19
Pr4zz4 <Prazza@Marks-MacBook-Pro.local>
2014-07-09 20:28:12 -0500

    Added jQuery exercise

--- friends.js
+++ friends.js
@@ -1,21 +1,43 @@
-// Add the names of everyone at your table   
-var friends = ['Alex', 'Elyse', 'Mae', 'Sartaj'];
+$( document ).ready(function() {
+  // Add the names of everyone at your table   
+	var friends = ['Alex', 'Elyse', 'Mae', 'Sartaj'];
 
-var list = "<ul>"
+	var list = "";
 
-// Create your for loop here
-// HINT: the string should look like '<li>' + friends[x] + "</li>" + ....
+	// Create your for loop here
+	// HINT: the string should look like '<li>' + friends[x] + "</li>" + ....
 
-for(var i = 0; i < friends.length; i++) {
-	list += "<li>"+ friends[i] + "</li> ";
+	for(var i = 0; i < friends.length; i++) {
+		list += "<li>"+ friends[i] + '<button data-id="' + i + '">remove</button> '+'</li>' ;
+	}
 
-}
+	var removeFriend = funciton(index) {
+		friends.splice(index, 1);
+		
+	}
+
+	list = list + "";
+
+	//
+
+	$("#namebutton").on('click', function(e) {
+		e.preventDefault();
+		name = $("#addname").val();
+		list += "<li>"+ name + '<button data-id="' + name + '">remove</button> '+'</li>';
+		$('#list').html(list);
+	});
+
+	//
+
+	$(this).on('click', function() {
+		removeFriend($(this).attr("data-id");
+	}
 
 
+	
 
-list = list + '</ul>'
-
-console.log(list);
-console.log($('.output'))
-// Use jQuery to select the body tag on the index and append the finish table string
-$('.output').append(list);
+	console.log(list);
+	console.log($('.output'));
+	// Use jQuery to select the body tag on the index and append the finish table string
+	$('#list').html(list);
+});
